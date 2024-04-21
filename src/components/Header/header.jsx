93b6625e43917348c5046b1d78b054c5fc4a2df2
Suetaforever7 <blekrasha62@gmail.com>
2024-04-21 14:21:06 +0500

    connect to the github

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,15 @@
+import "./header.css"
+
+function header() {
+  return (
+    <div className="header">
+<div className="container">
+    <div className="navbar">
+        <div className="navbar-line"></div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default header
